6a319440063290ef9473f94968cb3d7a82750305
Gareth Darby <Gareth.Darby@findel-education.co.uk>
2019-10-10 10:29:29 +0100

    Some further tweaks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1446,8 +1446,8 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 185
-#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_HOTEND 205
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
