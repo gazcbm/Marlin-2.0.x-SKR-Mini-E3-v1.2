44e10810ddfda1cf1d601334efd025d3e5a527fd
GazCBM <gareth.darby@outlook.com>
2019-10-24 13:07:47 +0100

    Speed settings + 3x3 grid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1200,7 +1200,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 7
+  #define GRID_MAX_POINTS_X 3  //WAS 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
